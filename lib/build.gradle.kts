666277c943b32b73f9cff13bfd2fcffc3570a3b3
zjun <565061763@qq.com>
2025-05-06 15:11:12 +0800

    remove iosSimulatorArm64

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
   listOf(
     iosX64(),
     iosArm64(),
-    iosSimulatorArm64()
+//    iosSimulatorArm64()
   ).forEach {
     it.binaries.framework {
       baseName = "com_sd_lib_kmp_compose_systemui"
