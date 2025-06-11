2935ba3b2e9226257909d77106e96e0299200908
zjun <565061763@qq.com>
2025-06-11 14:54:36 +0800

    java 11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
   androidTarget {
     publishLibraryVariants("release")
     compilerOptions {
-      jvmTarget.set(JvmTarget.JVM_1_8)
+      jvmTarget.set(JvmTarget.JVM_11)
     }
   }
 
@@ -46,7 +46,7 @@ android {
     minSdk = 21
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
   }
 }
